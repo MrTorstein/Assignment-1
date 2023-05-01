1a292f4b9dc158ae8a66e803631573182848cc50
Torstein S. Ølberg <mr.torstein@gmail.com>
2023-05-01 02:51:37 +0200

    New test

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ function func() {
 */
 const div = document.createElement("div");
 div.setAttribute("id", "div1");
+div.setAttribute('style', 'white-space: pre;');
 const input = document.createElement("input");
 const button = document.createElement("button");
 
@@ -19,17 +20,15 @@ document.body.appendChild(input);
 document.body.appendChild(button);
 
 function Get_input_content() {
-    let meny;
-    function Lagre_meny(tekst) {meny = tekst;}
     språk = input.value;
     input.value = "";
+    let meny;
     
     if (språk == "Norsk") {
-        fetch("./Poeng.txt").then(innfil => innfil.text()).then(fil_tekst => Lagre_meny(fil_tekst));
+        fetch("./Poeng.txt").then(innfil => innfil.text()).then(fil_tekst => meny = fil_tekst);
         div.textContent = meny;
     }
     else {
-        div.setAttribute('style', 'white-space: pre;');
         div.textContent = div.textContent + "\r\nThe submitted text is not a recognised language. Please enter a recognised one."
     }
 };
