a76ec89e83ef023af4152d76b9da7b844a29ccee
Torstein S. Ølberg <mr.torstein@gmail.com>
2023-05-01 02:18:39 +0200

    Test

--- test.js
+++ test.js
@@ -1,4 +1,37 @@
+/*
 function func() {
     fetch("./Poeng.txt").then(x => x.text()).then(y => let varible = y);
     alert(varible);
 };
+*/
+const div = document.createElement("div");
+div.setAttribute("id", "div1");
+const input = document.createElement("input");
+const button = document.createElement("button");
+
+let språk;
+
+div.textContent = "Enter a desired language. English or Norwegian.";
+button.textContent = "Submit";
+
+document.body.appendChild(div);
+document.body.appendChild(input);
+document.body.appendChild(button);
+
+function Get_input_content() {
+    let meny;
+    function Lagre_meny(tekst) {meny = tekst;}
+    språk = input.value;
+    input.value = "";
+    
+    if (språk == "Norsk") {
+        fetch("./Poeng.txt").then(innfil => innfil.text()).then(fil_tekst => Lagre_meny(fil_tekst));
+        div.textContent = meny;
+    }
+    else {
+        div.setAttribute('style', 'white-space: pre;');
+        div.textContent = div.textContent + "\r\nThe submitted text is not a recognised language. Please enter a recognised one."
+    }
+};
+button.onclick = Get_input_content;
+
